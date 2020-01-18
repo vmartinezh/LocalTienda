eef683c2e9ea13ec6761188cfeddb91ef5409f84
vmartinezh <v.martinezh@alumnos.duoc.cl>
2020-01-18 16:36:20 -0300

    parte VII

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Prenda, Colores, Marcas,Tallas
 
 # Create your views here.
+def index(request):
+
+    num_prenda= Prenda.objects.all().count()
+    num_colores= Colores.objects.all().count()
+
+
+    return render(
+        request,
+        'index.html',
+        context={'num_prenda': num_prenda, 'num_colores': num_colores},
+    )
--- localtienda/settings.py
+++ localtienda/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalogo',
+    'catalogo.apps.CatalogoConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'localtienda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
