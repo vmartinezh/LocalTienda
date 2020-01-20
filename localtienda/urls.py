3dc861322178a46852cd5112600ebf5e2a536573
vmartinezh <v.martinezh@alumnos.duoc.cl>
2020-01-20 11:44:44 -0400

    accounts

--- localtienda/urls.py
+++ localtienda/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('catalogo/', include('catalogo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
